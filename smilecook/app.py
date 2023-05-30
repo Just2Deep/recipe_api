9890f19e138f199eb2117b1cb97f59e7877e4ccf
Just2Deep <deepakrajsrs@gmail.com>
2023-05-30 18:13:30 +0530

    added user creation api

--- smilecook/app.py
+++ smilecook/app.py
@@ -3,9 +3,9 @@ from flask_migrate import Migrate
 from flask_restful import Api
 from config import Config
 from extensions import db
-from models.user import User
 
 from resources.recipe import RecipeResource, RecipeListResource, RecipePublishResource
+from resources.user import UserListResource
 
 
 def create_app():
@@ -28,6 +28,7 @@ def register_resources(app):
     api.add_resource(RecipeListResource, "/recipes")
     api.add_resource(RecipeResource, "/recipes/<int:recipe_id>")
     api.add_resource(RecipePublishResource, "/recipes/<int:recipe_id>/publish")
+    api.add_resource(UserListResource, "/users")
 
 
 if __name__ == "__main__":
