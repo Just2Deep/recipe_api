13a55f501d79636fceb5209dec2f9ba31e667f34
Just2Deep <deepakrajsrs@gmail.com>
2023-05-30 12:19:04 +0530

    updated DELETE endpoint

--- app.py
+++ app.py
@@ -28,11 +28,17 @@ recipes = [
 
 @app.route("/recipes", methods=["GET"])
 def get_recipes():
+    """
+    returns all the available recipes
+    """
     return jsonify({"data": recipes})
 
 
 @app.route("/recipes/<int:recipe_id>", methods=["GET"])
 def get_recipe(recipe_id):
+    """
+    checks for a specific recipe based on recipe id and returns it if present.
+    """
     if recipe := next(
         (recipe for recipe in recipes if recipe["id"] == recipe_id), None
     ):
@@ -43,6 +49,9 @@ def get_recipe(recipe_id):
 
 @app.route("/recipes", methods=["POST"])
 def create_recipe():
+    """
+    this endpoint allows you to create a new recipe.
+    """
     data = request.json
     name = data.get("name")
     description = data.get("description")
@@ -60,6 +69,9 @@ def create_recipe():
 
 @app.route("/recipes/<int:recipe_id>", methods=["PUT"])
 def update_recipe(recipe_id):
+    """
+    this endpoint allows you to update an already existing recipe.
+    """
     if recipe := next(
         (recipe for recipe in recipes if recipe["id"] == recipe_id), None
     ):
@@ -69,6 +81,17 @@ def update_recipe(recipe_id):
         )
 
         return jsonify(recipe)
+
+    return jsonify({"message": "recipe not found"}), HTTPStatus.NOT_FOUND
+
+
+@app.route("/recipes/<int:recipe_id>", methods=["DELETE"])
+def delete_recipe(recipe_id):
+    if recipe := next(
+        (recipe for recipe in recipes if recipe["id"] == recipe_id), None
+    ):
+        recipes.remove(recipe)
+        return jsonify({"message": "deleted content"}), HTTPStatus.NO_CONTENT
 
     return jsonify({"message": "recipe not found"}), HTTPStatus.NOT_FOUND
 
